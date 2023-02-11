e2e879725b9d922f95f6ef255bf690dbb13cb279
nastasi <nastasi.gost@mail.ru>
2023-02-11 20:40:05 +0300

    4.1

--- DZ4/script.js
+++ DZ4/script.js
@@ -9,28 +9,28 @@ for (let index = 0; index < digit.length; index++) {
     else console.log (`${digit[index]} - нечётное число`);
 }
 
-const digit = [1, 2, 3, 4, 5, 6, 7];
-digits.splice (3, 2);
-console.log(digit);
+const array = [1, 2, 3, 4, 5, 6, 7];
+array.splice (3, 2);
+console.log(array);
 
-const digit = [];
+const digits = [];
 for (let index = 0; index < 5; index++) {
    let number = (Math.random()*10);
-    digit[index] = parseInt (number);
+    digits[index] = parseInt (number);
 }
-console.log(digit);
+console.log(digits);
 
 let digSum = 0;
-for (let index = 0; index < digit.length; index++) {
-    digSum += digit[index];
+for (let index = 0; index < digits.length; index++) {
+    digSum += digits[index];
 }
 console.log(digSum);
 
-let min = Math.min.apply (null, digit);
+let min = Math.min.apply (null, digits);
 console.log(min);
 
-for (let index = 0; index < digit.length; index++) {
-    if (digit[index] == 3) {
-        console.log(digit[index]);
+for (let index = 0; index < digits.length; index++) {
+    if (digits[index] == 3) {
+        console.log(digits[index]);
     }
 }
